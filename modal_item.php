3663bf31f730195ee6df89b78e91d31dee20290d
Meycko Canoy <147495572+Meyckooo@users.noreply.github.com>
2026-08-24 17:12:04 +0800

    Update UserAdminPage
    08/24/2026

--- modal_item.php
+++ modal_item.php
@@ -11,6 +11,7 @@ $base_path = './';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modal Item</title>
     <link rel="stylesheet" href="<?php echo $base_path; ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo $base_path; ?>assets/css/media.css">
 </head>
 
 <body>
